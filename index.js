2610548e69ac9f671fd6969c63a3e4c1408e0beb
moonlightvfx <contactwithdavee@gmail.com>
2025-03-05 02:14:53 +0100

    index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Listening to skittle: http://localhost:${port}`);
+  console.log(`🔗 Powered By skittle_chan_yt`);
 });
 
 
