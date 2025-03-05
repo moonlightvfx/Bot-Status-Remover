64ecebf1f7b0fb1138188619def2adfe37cc0c17
moonlightvfx <contactwithdavee@gmail.com>
2025-03-05 02:07:45 +0100

    index.js

--- index.js
+++ index.js
@@ -59,7 +59,6 @@ function updateStatusAndSendMessages() {
 
 client.once('ready', () => {
   console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
   console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2025`);
   updateStatusAndSendMessages();
 
